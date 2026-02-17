2202d737468cb3fe985051bec53494008de7301e
pradnyajadhav1708-dot <pradnyajadhav1708@gmail.com>
2026-02-17 13:10:03 +0530

    Add files via upload

---
+++ Exp3/nested.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Feb 17 12:51:03 2026
+
+@author: User
+"""
+
+n =int(input("Enter number of rows:"))
+
+for i in range(1,n+1):
+    for j in range (1,i+1):
+        print(j,end=" ")
+    print()
+    
