a0779fa966281733bfffb7523ca4a04658f611c8
pradnyajadhav1708-dot <pradnyajadhav1708@gmail.com>
2026-03-10 13:58:40 +0530

    Add files via upload

---
+++ Exp 4/EXP 4 ex 3.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Mar 10 13:48:35 2026
+
+@author: Pradnya Jadhav
+"""
+
+marks = [75, 95, 90, 87, 78]
+
+average = sum(marks) / len(marks)
+topper = max(marks)
+
+print("Average Marks:", average)
+print("Topper Marks:", topper)
