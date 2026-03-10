d23ad11187bff9323d646362a6c3c362fe6eeea0
pradnyajadhav1708-dot <pradnyajadhav1708@gmail.com>
2026-03-10 14:00:18 +0530

    Add files via upload

---
+++ Exp 4/EXP 4 ex 7.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Mar 10 13:56:33 2026
+
+@author: User
+"""
+
+numbers = [9876543210, 9123456780, 9876543210, 9988776655, 9123456780]
+
+unique_numbers = list(set(numbers))
+
+print("Unique Numbers:", unique_numbers)
