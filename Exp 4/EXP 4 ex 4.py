3e4a6465aeccd66aeb05da443de1abbd0ef54d49
pradnyajadhav1708-dot <pradnyajadhav1708@gmail.com>
2026-03-10 13:59:12 +0530

    Add files via upload

---
+++ Exp 4/EXP 4 ex 4.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Mar 10 13:51:25 2026
+
+@author: Pradnya Jadhav
+"""
+
+prices = [300, 220, 95, 400, 70]
+
+total_bill = sum(prices)
+
+print("Total Bill:", total_bill)
+
