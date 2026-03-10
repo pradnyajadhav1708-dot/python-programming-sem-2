f3bfc22ef8e897fda1e7dc448f3ad9533b21d193
pradnyajadhav1708-dot <pradnyajadhav1708@gmail.com>
2026-03-10 13:59:43 +0530

    Add files via upload

---
+++ Exp 4/EXP 4 ex 5.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Mar 10 13:54:14 2026
+
+@author: Pradnya Jadhav
+"""
+
+feedback = "Great service and fast delivery"
+
+reversed_feedback = feedback[::-1]
+
+print("Reversed Feedback:", reversed_feedback)
