a0449e87db6c486492edfaf9d18121854b52beec
pradnyajadhav1708-dot <pradnyajadhav1708@gmail.com>
2026-03-17 13:10:24 +0530

    Add files via upload

---
+++ EXP 5 Act 3.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Mar 17 12:44:32 2026
+
+@author: Pradnya Jadhav
+"""
+books = {"Math": 300, "Physics": 400, "Chemistry": 350}
+
+# Update price
+books["Physics"] = 450
+
+print(books)
