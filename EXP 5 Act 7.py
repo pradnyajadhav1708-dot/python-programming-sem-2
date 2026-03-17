9de110be1caebe6186cd0ae5d1360e3926fb8377
pradnyajadhav1708-dot <pradnyajadhav1708@gmail.com>
2026-03-17 13:19:51 +0530

    Add files via upload

---
+++ EXP 5 Act 7.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Mar 17 13:05:02 2026
+
+@author: Pradnya Jadhav
+"""
+
+# Followers of two accounts
+account1 = {"A", "B", "C", "D"}
+account2 = {"C", "D", "E", "F"}
+
+common_followers = account1 & account2
+
+print("Common followers:", common_followers)
+
