6e8606d23557b95bac8d18fd36e76cb0c1b69924
pradnyajadhav1708-dot <pradnyajadhav1708@gmail.com>
2026-03-17 13:10:50 +0530

    Add files via upload

---
+++ EXP 5 Act 4.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Mar 17 12:56:01 2026
+
+@author: Pradnya Jadhav
+"""
+# Roll numbers of two classes
+classA = {1, 2, 3, 4, 5}
+classB = {4, 5, 6, 7}
+
+# Common students
+common = classA.intersection(classB)
+
+print("Students present in both classes:", common)
+
